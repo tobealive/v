a52913fc3d584111d85b690ddb2bf509167ae799
Turiiya <tobmoj@web.de>
2024-04-26 02:30:17 +0200

    wip
    s

--- vlib/v/util/diff/diff_test.v
+++ vlib/v/util/diff/diff_test.v
@@ -1,11 +1,12 @@
 import v.util.diff
 import os
+import term
 
 const tdir = os.join_path(os.vtmp_dir(), 'diff_test')
 
 fn testsuite_begin() {
 	os.find_abs_path_of_executable('diff') or {
-		eprintln('> skipping test, since this test requires `diff` to be installed')
+		eprintln('> skipping test `${@FILE}`, since this test requires `diff` to be installed')
 		exit(0)
 	}
 	os.mkdir_all(tdir)!
@@ -44,40 +45,55 @@ fn test_compare_files() {
 	assert res.contains("+\tlicense: 'MIT'"), res
 	// Test deprecated
 	assert res == diff.color_compare_files('diff', p1, p2)
+	// Test again using `find_working_diff_command()`.
+	assert res == diff.color_compare_files(diff.find_working_diff_command()!, p1, p2)
 
-	// Test adding custom options to the command.
-	res = diff.compare_files(p1, p2, args: ['-U 2', '-i', '--color=always'].join(' '))!
+	// Test custom options.
+	res = diff.compare_files(p1, p2, args: ['-U 2', '-i'].join(' '))!
 	assert !res.contains("+\tname: 'foo'"), res
 	assert res.contains("-\tversion: '0.0.0'"), res
 	assert res.contains("+\tversion: '0.1.0'"), res
 	assert res.contains("+\tlicense: 'MIT'"), res
 	// Test deprecated
-	assert res == diff.color_compare_files('diff --ignore-case', p1, p2)
+	assert res == term.strip_ansi(diff.color_compare_files('diff --ignore-case', p1, p2))
 
-	// Test again using `find_working_diff_command()`.
+	// Test options via env variable.
 	os.setenv('VDIFF_TOOL', 'diff', true)
-	res = diff.compare_files(p1, p2, cmd: .from_env)!
-	assert res.contains("-\tversion: '0.0.0'"), res
-	assert res.contains("+\tversion: '0.1.0'"), res
-	assert res.contains("+\tlicense: 'MIT'"), res
-	// Test deprecated
-	assert res == diff.color_compare_files(diff.find_working_diff_command()!, p1, p2)
-
-	// Test adding a flag via env flag.
-	os.setenv('VDIFF_OPTIONS', '--ignore-case -U 2 --color=always', true)
-	res = diff.compare_files(p1, p2, cmd: .from_env)!
+	os.setenv('VDIFF_OPTIONS', '--ignore-case -U 2', true)
+	res = diff.compare_files(p1, p2, allow_env_overwrite: true)!
 	assert !res.contains("+\tname: 'foo'"), res
 	assert res.contains("-\tversion: '0.0.0'"), res
 	assert res.contains("+\tversion: '0.1.0'"), res
 	assert res.contains("+\tlicense: 'MIT'"), res
 	// Test deprecated
 	os.setenv('VDIFF_OPTIONS', '--ignore-case', true)
-	assert res == diff.color_compare_files(diff.find_working_diff_command()!, p1, p2)
+	assert res == term.strip_ansi(diff.color_compare_files(diff.find_working_diff_command()!,
+		p1, p2))
+
+	// Test custom option that interferes with default options.
+	res = diff.compare_files(p1, p2, args: '--side-by-side')!
+	assert res.match_glob("*version: '0.0.0'*|*version: '0.1.0'*"), res
+
+	// Test custom diff command.
+	/* $if windows {
+		// Test windows default `fc`. TODO: enable when its os.execute output can be read.
+		res = diff.compare_files(p1, p1, cmd: .fc)!
+		assert res.contains('FC: no differences encountered')
+		res = diff.compare_files(p1, p2, cmd: .fc, args: '/b')!
+		assert res.contains('FC: ABCD longer than abc')
+	} */
+}
+
+fn test_compare_string() {
+	mut res := diff.compare_text('abc', 'abcd')!
+	assert res.contains('-abc'), res
+	assert res.contains('+abcd'), res
+	assert !res.contains('No newline at end of file'), res
 }
 
 fn test_coloring() {
 	if os.execute('diff --color=always').output.starts_with('diff: unrecognized option') {
-		eprintln('> skipping test, since `diff` does not support --color=always')
+		eprintln('> skipping test `${@FN}`, since `diff` does not support --color=always')
 		return
 	}
 	f1 := 'abc\n'
